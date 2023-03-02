c4f5ec48630f07060f9fa6bb8d8ad966679ac175
EugeneAndroschuk <enet2611@gmail.com>
2023-03-03 00:16:08 +0200

    логика

---
+++ src/scripts/read.js
@@ -0,0 +1,109 @@
+const form = document.getElementById('read-search-form');
+const search = document.getElementById('read-input');
+const cardList = document.getElementById('ul-gallery');
+const parseBtn = document.getElementById('parseBtn');
+let articles = [];
+const article = {
+  readDate: '',
+  imgurl: '',
+  headline: '',
+  snippet: '',
+  weburl: '',
+};
+
+form.addEventListener('submit', onSubmit);
+window.addEventListener('click', onClick);
+parseBtn.addEventListener('click', onParse);
+
+function onSubmit(e) {
+  const API_KEY = '6NeZFvbRUjOlM3jxAALEHJAyoskEi5UY';
+  const querySearch = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search.value}&api-key=${API_KEY}`;
+
+  e.preventDefault();
+
+  fetch(querySearch)
+    .then(response => response.json())
+    .then(data => {
+      let i = 1;
+      for (let a of data.response.docs) {
+        const obj = {};
+
+        const date = new Date();
+        let convertDate = '';
+        if (i % 2 === 0)
+          convertDate = `${String(date.getDate()).padStart(2, 0)}/${String(
+            date.getMonth() + 1
+          ).padStart(2, 0)}/${String(date.getFullYear())}`;
+        else
+          convertDate = `${String(date.getDate() - 1).padStart(2, 0)}/${String(
+            date.getMonth() + 1
+          ).padStart(2, 0)}/${String(date.getFullYear())}`;
+
+        obj.readDate = convertDate;
+        obj.imgurl = a.multimedia[0].url;
+        obj.headline = a.headline.main;
+        obj.snippet = a.snippet;
+        obj.weburl = a.web_url;
+        articles.push(obj);
+
+        i += 1;
+      }
+      console.log('получился массив статей    ', articles);
+
+      try {
+        localStorage.setItem('have read', JSON.stringify(articles));
+      } catch (err) {
+        console.error(err);
+      }
+    });
+}
+
+function onParse() {
+  try {
+    const unparsed = localStorage.getItem('have read');
+    const parsed = JSON.parse(unparsed);
+    if (parsed !== null) {
+      renderMarkup(parsed);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function renderMarkup(array) {
+  let cardMarkup = '';
+  const uniqueDates = array
+    .flatMap(obj => obj.readDate)
+    .filter((course, index, array) => array.indexOf(course) === index);
+  for (let date of uniqueDates) {
+    const cardMarkupLi = `<li class="cards">
+            <h2>View date ${date}</h2>`;
+    const cardMarkupDiv = array
+      .filter(obj => obj.readDate === date)
+      .map(obj => {
+        let src = ``;
+        if (obj.imgurl === 0) src = './images/img.jpeg';
+        else src = `https://www.nytimes.com/${obj.imgurl}`;
+        return `<div class="card">
+                      <img src="${src}" alt="" />
+                      <h3>${obj.headline}</h3>
+                      <p class="info-item">${obj.snippet}</p>
+                      <p class="info-item">${obj.weburl}</p>
+            </div>`;
+      });
+    cardMarkup += cardMarkupLi + cardMarkupDiv.join('') + '</li>';
+  }
+  cardList.innerHTML = cardMarkup;
+}
+
+function onClick(e) {
+  console.log(e.target.tagName);
+  console.log(e.target.children);
+  if (e.target.tagName === 'H2') {
+    let cont = e.target.parentNode.querySelectorAll('div');
+
+    for (let item of cont) item.classList.toggle('hidden');
+
+    console.log(cont);
+  }
+}
