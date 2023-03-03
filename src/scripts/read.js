3dc7c3407a8f4ca7f08e13823426ccf07de47f61
EugeneAndroschuk <enet2611@gmail.com>
2023-03-03 18:55:22 +0200

    arrows

--- src/scripts/read.js
+++ src/scripts/read.js
@@ -3,13 +3,13 @@ const search = document.getElementById('read-input');
 const cardList = document.getElementById('ul-gallery');
 const parseBtn = document.getElementById('parseBtn');
 let articles = [];
-const article = {
-  readDate: '',
-  imgurl: '',
-  headline: '',
-  snippet: '',
-  weburl: '',
-};
+// const article = {
+//   readDate: '',
+//   imgurl: '',
+//   headline: '',
+//   snippet: '',
+//   weburl: '',
+// };
 
 form.addEventListener('submit', onSubmit);
 window.addEventListener('click', onClick);
@@ -29,17 +29,13 @@ function onSubmit(e) {
         const obj = {};
 
         const date = new Date();
-        let convertDate = '';
-        if (i % 2 === 0)
-          convertDate = `${String(date.getDate()).padStart(2, 0)}/${String(
-            date.getMonth() + 1
-          ).padStart(2, 0)}/${String(date.getFullYear())}`;
-        else
-          convertDate = `${String(date.getDate() - 1).padStart(2, 0)}/${String(
-            date.getMonth() + 1
-          ).padStart(2, 0)}/${String(date.getFullYear())}`;
 
-        obj.readDate = convertDate;
+        if (i % 2 === 0) obj.readDate = date.getTime();
+        else if (i % 3 === 0) obj.readDate = date.getTime() - 180000000;
+        else obj.readDate = date.getTime() - 360000000;
+
+        console.log(obj.readDate);
+        console.log(i);
         obj.imgurl = a.multimedia[0].url;
         obj.headline = a.headline.main;
         obj.snippet = a.snippet;
@@ -48,6 +44,7 @@ function onSubmit(e) {
 
         i += 1;
       }
+
       console.log('получился массив статей    ', articles);
 
       try {
@@ -72,27 +69,57 @@ function onParse() {
 
 function renderMarkup(array) {
   let cardMarkup = '';
-  const uniqueDates = array
-    .flatMap(obj => obj.readDate)
-        .filter((course, index, array) => array.indexOf(course) === index);
-    
-    for (let date of uniqueDates) {
-      
+
+  // -------сортируем даты по убыванию и выделяем уникальные даты---------
+
+  const initialDates = array.map(obj => obj.readDate);
+  const descendingDates = [...initialDates]
+    .sort((a, b) => b - a)
+    .map(dateUnix => {
+      const dateStamp = new Date(dateUnix);
+
+      return `${String(dateStamp.getDate()).padStart(2, 0)}/${String(
+        dateStamp.getMonth() + 1
+      ).padStart(2, 0)}/${String(dateStamp.getFullYear())}`;
+    });
+  const uniqueDates = descendingDates.filter(
+    (course, index, array) => array.indexOf(course) === index
+  );
+
+  //------- создаем массив объектов с датой в формате year/month/day---------
+
+  const arrayConverDates = array.reduce((acc, obj) => {
+    const newObj = {};
+    const dateStamp = new Date(obj.readDate);
+
+    newObj.readDate = `${String(dateStamp.getDate()).padStart(2, 0)}/${String(
+      dateStamp.getMonth() + 1
+    ).padStart(2, 0)}/${String(dateStamp.getFullYear())}`;
+    newObj.imgurl = obj.imgurl;
+    newObj.headline = obj.headline;
+    newObj.snippet = obj.snippet;
+    newObj.weburl = obj.weburl;
+    acc.push(newObj);
+    return acc;
+  }, []);
+
+  //------- рендерим разметку по отсортированным уникальным датам---------
+
+  for (let date of uniqueDates) {
     const cardMarkupLi = `<li class="cards">
             <h2 class="cards-date">${date}
-            <img class="d" src="https://w7.pngwing.com/pngs/475/618/png-transparent-arrow-computer-icons-bar-chart-arrow-angle-chevron-svg-filter-effects-thumbnail.png" alt="" width="9px" height="15px"/>
-            <svg class="arrow arrow-up" width="20" height="20">
-                  <use href="/src/images/sprite.svg#cross-icon"></use>
-                </svg>
-                <svg class="arrow arrow-down" width="9" height="15">
-                  <use href="/src/images/sprite.svg#icon-arrow-up"></use>
-                </svg>
-                </h2>
+            <svg class="arrow-up" width="15" height="9" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <path d="M1.762 9 0 7.287 7.5 0 15 7.287 13.238 9 7.5 3.437 1.763 9Z" fill="#111321"/>
+            </svg>
+            <svg class="arrow-down" width="15" height="9" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <path d="M1.762 0 0 1.713 7.5 9 15 1.713 13.238 0 7.5 5.563 1.763 0Z" fill="#111321"/>
+            </svg>
+            </h2>
                 `;
-    const cardMarkupDiv = array
+    const cardMarkupDiv = arrayConverDates
       .filter(obj => obj.readDate === date)
       .map(obj => {
-          const src = `https://www.nytimes.com/${obj.imgurl}`;
+        const src = `https://www.nytimes.com/${obj.imgurl}`;
         return `<div class="card">
                       <img src="${src}" alt="" />
                       <h3>${obj.headline}</h3>
@@ -110,9 +137,6 @@ function onClick(e) {
   console.log(e.target.children);
   if (e.target.tagName === 'H2') {
     let cont = e.target.parentNode.querySelectorAll('div');
-
     for (let item of cont) item.classList.toggle('hidden');
-
-    console.log(cont);
   }
 }
