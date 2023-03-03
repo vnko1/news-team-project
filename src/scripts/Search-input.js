92b1986fae87124126843d727eb472f053e0dc80
Vladislav <bakalavrat.789@gmail.com>
2023-03-03 19:00:42 +0200

    Update_

--- src/scripts/Search-input.js
+++ src/scripts/Search-input.js
@@ -1,6 +1,6 @@
 // import serviceApi from './api-key';
 
-// const formEl = document.querySelector('#search-form');
+const formEl = document.querySelector('#search-form');
 const articlesContainer = document.querySelector('.js-articles-container');
 
 // // const serviceApi = new ApiService();
@@ -8,33 +8,33 @@ const articlesContainer = document.querySelector('.js-articles-container');
 
 // formEl.addEventListener('submit', onSearch);
 
-// function onSearch(e) {
-//   e.preventDefault();
-//   // serviceApi.query = e.currentTarget.elements.searchQuery.value;
-//   const form = e.currentTarget;
-//   console.log(form);
-//   const value = form.elements.searchQuery.value.trim();
+function onSearch(e) {
+  e.preventDefault();
+  serviceApi.query = e.currentTarget.elements.searchQuery.value;
+  const form = e.currentTarget;
+  // console.log(form);
+  const value = form.elements.searchQuery.value.trim();
 
-//   // if (serviceApi.query === '') {
-//   //   return alert('Введи что-то нормальное');
-//   // }
-//   serviceApi(value)
-//     .then(({ articles }) => {
-//       if (articles.length === 0) throw new Error('No data');
+  // if (serviceApi.query === '') {
+  //   return alert('Введи что-то нормальное');
+  // }
+  serviceApi(value)
+    .then(({ articles }) => {
+      if (articles.length === 0) throw new Error('No data');
 
-//       return articles.reduce(
-//         (markup, article) => createMarkup(article) + markup,
-//         ''
-//       );
-//     })
-//     .then(updateNewsList)
-//     .catch(onError)
-//     .finally(() => form.reset());
-// }
+      return articles.reduce(
+        (markup, article) => createMarkup(article) + markup,
+        ''
+      );
+    })
+    .then(updateNewsList)
+    .catch(onError)
+    .finally(() => form.reset());
+}
 
-// function updateNewsList(markup) {
-//   articlesContainer.innerHTML = markup;
-// }
+function updateNewsList(markup) {
+  articlesContainer.innerHTML = markup;
+}
 
 // // serviceApi.resetPage();
 // // fetchArticles();
@@ -81,7 +81,7 @@ const res = fetch(BASE_URL)
   .then(data => {
     // console.log(data);
     createDivMarkup(data.results);
-      console.log(data.results);
+      // console.log(data.results);
   })
   .catch(error => {
     console.log(error);
@@ -110,12 +110,14 @@ function createMarkup(dataOfOneCard) {
   }
   return `
     <div class="card">
+    <div class="news-card__img"> 
     <img class="card__img" src="https://static01.nyt.com/${media}" alt="Заглушка" width="395" height="395"></img> 
       <h2 class="card__title" data-card-title>${title}</h2>
       <p class="card__description">${abstract}</p>
       <p class="card__news-create-date">${convertDate}</p>
       <a class="card__news-link" href="${url}" id="${id}">Read more</a>
       <button class="card__btn">Add to favorite</button>
+      <div>
     </div>
   `;
 }
@@ -126,3 +128,6 @@ function createMarkup(dataOfOneCard) {
 // короткий опис: abstract
 // дата: published_date
 // елемент read more: url
+// / Відображаємо лише 8 елементів
+//       const slicedData = data.response.docs.slice(0, 8);
+// ти береш дані які приходять у відповідь з апішки та обрізаєш їх до потрібної довжини
