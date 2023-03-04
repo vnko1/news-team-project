e92533bad55cbccbd640ae44a61edaee8e563812
Andrii Valenko <andreyvalenko@gmail.com>
2023-03-04 12:43:55 +0200

    s

---
+++ src/scripts/CommonFunctions.js
@@ -0,0 +1,57 @@
+import { fetchNews } from './fetchNews';
+
+const gallery = document.querySelector('.gallery-container');
+
+function renderNewsCards() {
+  //  cтворюємо новий масив
+  const renderData = [];
+  // отримуємо масив даних з екземпляру класу
+  const fetchData = fetchNews.getData();
+  // перебираємо маси та перші 8 елементів пушимо в renderData
+  for (let i = 0; i < fetchData.length; i++) {
+    if (i >= 8) break;
+    renderData.push(fetchData[i]);
+  }
+  // створюємо строку розмітки
+  const markUp = renderData.reduce((acc, el) => {
+    acc += `<div class="news-card" news-id="${el.id}">
+      <div class="news-card__img">
+        <p class="news-card__theme">${el.category}</p>
+        <img
+          class="news-card__item"
+          src="${el.imgUrl}"
+          alt="${el.imgDescr ? el.imgDescr : 'photo'}"
+          loading="lazy"
+          width="395"
+        />
+        <div class="news-card__favorite">
+          <label for="favorite" id="${
+            el.id
+          }" class="label-favorite">Add to favorite</label>
+          <input type="checkbox" class="input-favorite" id="favorite"/>
+        </div>
+      </div>
+      <h2 class="news-card__info-title">${el.title}</h2>
+      <p class="news-card__info-text">${
+        el.description.length > 180
+          ? el.description.slice(0, 180) + '...'
+          : el.description
+      }</p>
+      <div class="news-card__additional">
+        <p class="news-card__date">${el.pubDate}</p>
+        <a class="news-card__more" href="${el.url}" id="${el.id}"}>Read more</a>
+      </div>
+    </div>`;
+    return acc;
+  }, ``);
+  // додоємо створену розмітку в DOM
+  gallery.insertAdjacentHTML('beforeend', markUp);
+}
+
+function deleteNewsCards() {
+  gallery.innerHTML = '';
+  // const newsCards = fetchNews.getNodeChild();
+  // newsCards.forEach(el => el.remove());
+}
+
+export { renderNewsCards, deleteNewsCards };
--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,5 +1,6 @@
 import VanillaCalendar from '@uvarov.frontend/vanilla-calendar';
 import { fetchNews } from './fetchNews';
+import { renderNewsCards, deleteNewsCards } from './CommonFunctions';
 
 const calendar = new VanillaCalendar('#calendar');
 calendar.init();
@@ -8,7 +9,6 @@ const inputEl = document.querySelector('#date');
 const calendarContainer = document.querySelector('#calendar-container');
 const calendarEl = document.querySelector('#calendar');
 const gallery = document.querySelector('.gallery-container');
-
 const dateContainer = document.querySelector('.date-container');
 
 inputEl.addEventListener('click', onInputElClick);
@@ -92,56 +92,4 @@ function saveData(data) {
 
 function pushData(data) {
   fetchNews.addData(fetchNews.createObj(data));
-}
-
-function deleteNewsCards() {
-  gallery.innerHTML = '';
-  // const newsCards = fetchNews.getNodeChild();
-  // newsCards.forEach(el => el.remove());
-}
-
-function renderNewsCards() {
-  //  cтворюємо новий масив
-  const renderData = [];
-  // отримуємо масив даних з екземпляру класу
-  const fetchData = fetchNews.getData();
-  // перебираємо маси та перші 8 елементів пушимо в renderData
-  for (let i = 0; i < fetchData.length; i++) {
-    if (i >= 8) break;
-    renderData.push(fetchData[i]);
-  }
-  // створюємо строку розмітки
-  const markUp = data.reduce((acc, el) => {
-    acc += `<div class="news-card" news-id="${el.id}">
-      <div class="news-card__img">
-        <p class="news-card__theme">${el.category}</p>
-        <img
-          class="news-card__item"
-          src="${el.imgUrl}"
-          alt="${el.imgDescr ? el.imgDescr : 'photo'}"
-          loading="lazy"
-          width="395"
-        />
-        <div class="news-card__favorite">
-          <label for="favorite" id="${
-            el.id
-          }" class="label-favorite">Add to favorite</label>
-          <input type="checkbox" class="input-favorite" id="favorite"/>
-        </div>
-      </div>
-      <h2 class="news-card__info-title">${el.title}</h2>
-      <p class="news-card__info-text">${
-        el.description.length > 180
-          ? el.description.slice(0, 180) + '...'
-          : el.description
-      }</p>
-      <div class="news-card__additional">
-        <p class="news-card__date">${el.pubDate}</p>
-        <a class="news-card__more" href="${el.url}" id="${el.id}"}>Read more</a>
-      </div>
-    </div>`;
-    return acc;
-  }, ``);
-  // додоємо створену розмітку в DOM
-  gallery.insertAdjacentHTML('beforeend', markUp);
 }
