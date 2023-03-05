bd9b37037b0743e0f87c7e33bfa80e3ffe8d4d07
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-03-05 14:29:44 +0200

    s

--- src/scripts/Most-Popular.js
+++ src/scripts/Most-Popular.js
@@ -1,15 +1,17 @@
 import { fetchNews } from './FetchNews';
 
-const gallery = document.querySelector('.gallery-container');
+import {createObj, renderNewsCards} from './CommonFunctions'
+
+// const gallery = document.querySelector('.gallery-container');
 
 onLoad();
 
 async function onLoad() {
   const response = await fetchNews.fetchNewsByDate();
-  console.log(response);
-  // normalizeData(response);
+  // console.log(response.data.results);
+  normalizeData(response.data.results);
 
-  // renderNewsCards();
+  renderNewsCards();
   // fetchNews.setNodeChild(document.querySelectorAll('.news-card'));
 }
 
@@ -51,7 +53,7 @@ function pushData(
   id
 ) {
   fetchNews.addData(
-    fetchNews.createObj({
+    createObj({
       title,
       description,
       category,
@@ -64,46 +66,46 @@ function pushData(
   );
 }
 
-function renderNewsCards() {
-  const data = [];
-  const fetchData = fetchNews.getData();
+// function renderNewsCards() {
+//   const data = [];
+//   const fetchData = fetchNews.getData();
 
-  for (let i = 0; i < fetchData.length; i++) {
-    if (i >= 8) break;
-    data.push(fetchData[i]);
-  }
+//   for (let i = 0; i < fetchData.length; i++) {
+//     if (i >= 8) break;
+//     data.push(fetchData[i]);
+//   }
 
-  const markUp = data.reduce((acc, el) => {
-    acc += `<div class="news-card" news-id="${el.id}">
-      <div class="news-card__img">
-        <p class="news-card__theme">${el.category}</p>
-        <img
-          class="news-card__item"
-          src="${el.imgUrl}"
-          alt="${el.imgDescr ? el.imgDescr : 'photo'}"
-          loading="lazy"
-          width="395"
-        />
-        <div class="news-card__favorite">
-          <label for="favorite" class="label-favorite">Add to favorite</label>
-          <input type="checkbox" class="input-favorite" id="favorite" />
-        </div>
-      </div>
-      <h2 class="news-card__info-title">${el.title}</h2>
-      <p class="news-card__info-text">${
-        el.description.length > 180
-          ? el.description.slice(0, 180) + '...'
-          : el.description
-      }</p>
-      <div class="news-card__additional">
-        <p class="news-card__date">${el.pubDate}</p>
-        <a class="news-card__more" href="${el.url}">Read more</a>
-      </div>
-    </div>`;
-    return acc;
-  }, ``);
-  gallery.insertAdjacentHTML('beforeend', markUp);
-}
+//   const markUp = data.reduce((acc, el) => {
+//     acc += `<div class="news-card" news-id="${el.id}">
+//       <div class="news-card__img">
+//         <p class="news-card__theme">${el.category}</p>
+//         <img
+//           class="news-card__item"
+//           src="${el.imgUrl}"
+//           alt="${el.imgDescr ? el.imgDescr : 'photo'}"
+//           loading="lazy"
+//           width="395"
+//         />
+//         <div class="news-card__favorite">
+//           <label for="favorite" class="label-favorite">Add to favorite</label>
+//           <input type="checkbox" class="input-favorite" id="favorite" />
+//         </div>
+//       </div>
+//       <h2 class="news-card__info-title">${el.title}</h2>
+//       <p class="news-card__info-text">${
+//         el.description.length > 180
+//           ? el.description.slice(0, 180) + '...'
+//           : el.description
+//       }</p>
+//       <div class="news-card__additional">
+//         <p class="news-card__date">${el.pubDate}</p>
+//         <a class="news-card__more" href="${el.url}">Read more</a>
+//       </div>
+//     </div>`;
+//     return acc;
+//   }, ``);
+//   gallery.insertAdjacentHTML('beforeend', markUp);
+// }
 // const BASE_URL =
 //   'https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=6NeZFvbRUjOlM3jxAALEHJAyoskEi5UY';
 
