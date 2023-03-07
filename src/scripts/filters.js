94155ea89d704f97a48e77896d2ce75859a95c99
Eduard-Osheha <eduardoshega07@gmail.com>
2023-03-07 23:44:11 +0100

    final commitit

--- src/scripts/filters.js
+++ src/scripts/filters.js
@@ -20,6 +20,7 @@ Notify.init({
   timeout: 1500,
 });
 
+
 const galleryContainer = document.querySelector('.gallery-container');
 const categoryContainer = document.querySelector('.filters .container');
 const filterBtnsWrap = document.querySelector('.filters__button-wrap');
@@ -28,13 +29,33 @@ const filterCategory = document.querySelectorAll('.filters__button');
 const otherCategoriesBtn = document.querySelector(
   '.selected_container .filters__button'
 );
+console.log('💛💙💪  otherCategoriesBtn:', otherCategoriesBtn);
 
-// categoryContainer.addEventListener('click', onClickCategoryBtn);
+const dropdownBtn = document.querySelector('.category_btn');
+const dropdownMenu = document.querySelector('.category_dropdown');
+const categoryContainerCopy = document.querySelector('.category');
+const notDropdownBtnContainer = document.querySelector(
+  '.category_notdropdownbtn_container'
+);
+const body = document.querySelector('body');
+const nonDropdownBtn = document.querySelectorAll('.category_nondropdown_btn');
+
+
+
 filterBtnsWrap.addEventListener('click', onClickCategoryBtn);
 otherCategoriesBtn.addEventListener('click', onClickOthersBtn);
 selectedList.addEventListener('click', onClickOtherCategory);
+// window.addEventListener('click', () => {
 
-fetchCategoryNames();
+//   if (dropdownMenu.classList.contains('shown')) {
+//     dropdownMenu.classList.remove('shown');
+//   } else if (selectedList.classList.contains('shown')) {
+//     selectedList.classList.remove('shown');
+//   }});
+
+
+createFilters();
+
 
 //  це функція яка викликатиметься при натисканні на кнопку - фільтр,
 async function onClickCategoryBtn(e) {
@@ -81,12 +102,12 @@ async function onClickCategoryBtn(e) {
 }
 
 async function onClickOtherCategory(e) {
-  spinner.spin(document.body);
-  e.preventDefault();
-  selectedList.classList.remove('shown');
-  const query = e.target.textContent;
-  otherCategoriesBtn.innerText = query;
   try {
+    selectedList.classList.toggle('shown');
+    spinner.spin(document.body);
+    e.preventDefault();
+    const query = e.target.textContent;
+    otherCategoriesBtn.innerText = query;
     fetchNews.resetData();
     fetchNews.resetStorageData();
 
@@ -124,6 +145,157 @@ async function onClickOtherCategory(e) {
   }
 }
 
+function createSelectedList({ display_name }) {
+  return `<a class="selected__item" href="">${display_name}</a>`;
+}
+
+function appendSelectedList(murkupForSelectedList) {
+  selectedList.insertAdjacentHTML('afterbegin', murkupForSelectedList);
+}
+
+function onClickOthersBtn() {
+  selectedList.classList.toggle('shown');
+}
+
+
+
+function appendNotFoundImage() {
+  const image = document.createElement('img');
+  image.src = '';
+  image.alt = 'We haven’t found news from this category';
+  galleryContainer.append(image);
+}
+
+async function createFilters () {
+  try {
+    const results = await fetchNews.getCategoryNames();
+    const shortResults = results
+      .filter(result => result.display_name.length <= 10)
+      .sort();
+    
+
+    const categoriesAll = shortResults.map(({ section, display_name }) => {
+      return display_name;
+    });
+
+    const categoriesForTabletBtn = shortResults.reduce((acc, result) => {
+      if (shortResults.indexOf(result) <= 3) {
+        acc.push(result.display_name);
+      }
+      return acc;
+    }, []);
+
+    const categoriesForTabletList = shortResults.reduce((acc, result) => {
+      if (shortResults.indexOf(result) > 3) {
+        acc.push(result.display_name);
+      }
+      return acc;
+    }, []);
+
+    const categoriesForDesktopBtn = shortResults.reduce((acc, result) => {
+      if (shortResults.indexOf(result) <= 5) {
+        acc.push(result.display_name);
+      }
+      return acc;
+    }, []);    
+
+    const categoriesForDesktopList = shortResults.reduce((acc, result) => {
+      if (shortResults.indexOf(result) > 5) {
+        acc.push(result.display_name);
+      }
+      return acc;
+    }, []); 
+
+
+
+  const mediaQueryDesktop = window.matchMedia('(min-width: 1280px)')
+  const mediaQueryTablet = window.matchMedia('screen and (min-width: 768px) and (max-width: 1279px')  
+  const mediaQueryMobile = window.matchMedia('(max-width: 767px)');
+
+  // Register event listener
+  mediaQueryDesktop.addListener(renderDesktopCategories);
+  mediaQueryTablet.addListener(renderTabletCategories);
+mediaQueryMobile.addListener(renderMobileCategories);
+
+
+// Initial check
+renderDesktopCategories(mediaQueryDesktop);
+renderTabletCategories(mediaQueryTablet);
+renderMobileCategories(mediaQueryMobile);
+
+
+
+if (mediaQueryMobile.matches) {
+  renderMobileCategories(mediaQueryMobile);
+} else if (mediaQueryTablet.mathes) {
+  renderTabletCategories(mediaQueryTablet);
+} else {
+  renderDesktopCategories(mediaQueryDesktop)
+}
+
+async function renderDesktopCategories(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    dropdownBtn.firstChild.textContent = 'Others';
+    const markupBtn = categoriesForDesktopBtn.reduce((acc, category) => {
+      return (acc += createButtons(category));
+    }, '');
+    addButtons(markupBtn);
+    const markupList = categoriesForDesktopList.reduce((acc, category) => {
+      return (acc += createMarkup(category));
+    }, '');
+    addMarkup(markupList);
+  }
+}
+async function renderMobileCategories(e) {
+  if (e.matches) {
+  dropdownBtn.firstChild.textContent = 'Categories';
+  const markup = categoriesAll.reduce((acc, category) => {
+    return (acc += createMarkup(category));
+  }, '');
+  notDropdownBtnContainer.innerHTML = '';
+  addMarkup(markup);}
+  
+  }
+  async function renderTabletCategories(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    dropdownBtn.firstChild.textContent = 'Others';
+      const markupBtn = categoriesForTabletBtn.reduce((acc, category) => {
+        return (acc += createButtons(category));
+      }, '');
+      addButtons(markupBtn);
+
+      const markupList = categoriesForTabletList.reduce((acc, category) => {
+        return (acc += createMarkup(category));
+      }, '');
+
+      addMarkup(markupList);
+  }
+}
+
+  } catch (error) {
+console.log(error)
+  }
+}
+
+
+function createMarkup(category) {
+  return `<a class="category_dropdown_item selected__item" href="#">${category}</a>`;
+}
+
+function createButtons(category) {
+  return `<button class="category_btn category_nondropdown_btn filters__button" href="#">${category}</button>`;
+}
+
+function addMarkup(markup) {
+  dropdownMenu.innerHTML = `${markup}`;
+}
+
+function addButtons(markup) {
+  notDropdownBtnContainer.innerHTML = `${markup}`;
+}
+
 async function fetchCategoryNames() {
   try {
     const categoryResults = await fetchNews.getCategoryNames();
@@ -154,48 +326,4 @@ async function fetchCategoryNames() {
   } catch (error) {
     console.error(error.message);
   }
-}
-
-// async function createFilters (){
-//   try {
-//   const mediaQueryDesktop = window.matchMedia('(min-width: 1280px)')
-//   const mediaQueryTablet = window.matchMedia('(min-width: 768px) && (max-width: 1279px)');
-//   const mediaQueryMobile = window.matchMedia('(max-width: 767px)');
-
-
-// function handleViewportChange(e) {
-//   // Check if the media query is true
-//   if (e.matches) {
-//     // Then log the following message to the console
-//     Notify.info('Media query is allright'); 
-//   }
-//   Notify.failure('Media query is false')
-// }
-// // Register event listener
-// mediaQueryDesktop.addListener(handleTabletChange)
-
-// // Initial check
-// handleViewportChange(mediaQueryDesktop);
-//   } catch (error) {
-// console.log(error)
-//   }
-// }
-
-function createSelectedList({ display_name }) {
-  return `<a class="selected__item" href="">${display_name}</a>`;
-}
-
-function appendSelectedList(murkupForSelectedList) {
-  selectedList.insertAdjacentHTML('afterbegin', murkupForSelectedList);
-}
-
-function appendNotFoundImage() {
-  const image = document.createElement('img');
-  image.src = '';
-  image.alt = 'We haven’t found news from this category';
-  galleryContainer.append(image);
-}
-
-function onClickOthersBtn() {
-  selectedList.classList.add('shown');
 }
