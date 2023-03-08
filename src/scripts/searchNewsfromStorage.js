cb813c0b5071b3670bb9ab124ab394263ecdb76f
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-03-08 21:07:46 +0200

    ss

--- src/scripts/searchNewsfromStorage.js
+++ src/scripts/searchNewsfromStorage.js
@@ -1,26 +1,56 @@
 import {getStorageList} from './commonFunctions'
 
-const inputEl = document.querySelector('.search-input');
+// const inputEl = document.querySelector('.search-input');
 const formEl = document.getElementById('search-form');
 const galleryEl = document.querySelector('.gallery-container');
+
+console.log(galleryEl)
 
 formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault()
-console.log(e.target.elements)
+  const searchValue = e.target.elements.searchQuery.value;
+
+  const arr = getStorageList('favourites');
+
+  const newArrObj = arr.filter(obj => obj.category.toLowerCase().includes(searchValue.toLowerCase().trim()))
+// console.log(newArrObj)
+
+render(newArrObj)
 }
 
-// inputEl.addEventListener('change', inputChange)
+function render(arr) {
 
-// function inputChange(e) {
-//   console.log(e)
-// }
-
-console.log(getStorageList)
-const arr = getStorageList('favorite');
-console.log(arr)
-
-// arr.filter((obj)=>{
-
-// })
+  const markUp = arr.reduce((acc, el) => {
+    acc += `<div class="news-card" news-id="${el.id}">
+      <div class="news-card__img">
+        <p class="news-card__theme">${el.category}</p>
+        <img
+          class="news-card__item"
+          src="${el.img}"
+          alt="${el.alt ? el.alt : 'photo'}"
+          loading="lazy"
+          width="395"
+        />
+        <div class="news-card__favorite">
+          <label for="favorite" id="${
+            el.id
+          }" class="label-favorite">Add to favorite</label>
+          <input type="checkbox" class="input-favorite" id="favorite"/>
+        </div>
+      </div>
+      <h2 class="news-card__info-title">${el.title}</h2>
+      <p class="news-card__info-text">${el.descr.limit(180)}</p>
+      <div class="news-card__additional">
+        <p class="news-card__date">${el.dateArticle}</p>
+        <a class="news-card__more" href="${el.link}" id="${
+      el.id
+    }" target="_blank" rel="noreferrer noopener"}>Read more</a>
+      </div>
+    </div>`;
+    return acc;
+  }, ``);
+// console.log(markUp)
+  galleryEl.innerHTML = markUp;
+}
