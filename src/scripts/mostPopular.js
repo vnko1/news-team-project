61bac872bc7a2f2392b7d65a7a0ffefc23a52653
Andrey Valenko <102794464+vnko1@users.noreply.github.com>
2023-03-08 11:10:14 +0200

    Update mostPopular.js

--- src/scripts/mostPopular.js
+++ src/scripts/mostPopular.js
@@ -4,7 +4,7 @@ import {
   renderNewsCards,
   savePopularData,
   addClassesForCoincidencesMarkupAndStorage,
-} from './CommonFunctions';
+} from './сommonFunctions';
 import { paginationByQuery } from './pagination';
 
 onLoad();
